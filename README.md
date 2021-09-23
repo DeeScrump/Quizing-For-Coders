f15ef70eadf467fa23b28ce2f86d67f067e5741c
DCKII <dckornbluthii@bellsouth.net>
2021-09-23 00:01:30 -0400

    committing for submittal

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Quizing-For-Coders
 We will be presenting coders with some questions to quiz their knowledge of html, css and javascript.  Can you beat the top score?!
+
+Code does not work as of now. 
+
+I will RE-SUBMIT
--- quiz.js
+++ quiz.js
@@ -3,131 +3,132 @@ var timeCountDown = document.querySelector(".timer_counter");
 // var displayQuestions = document.querySelector(".quiz");
 var timer;
 var timerCounter;
-
-
-function showQuestions(){
-  // variable to store the HTML output
-  var output = [];
-
-  // for each question...
-  listOfQuestions.forEach(
-    (currentQuestion, questionNumber) => {
-
-      // variable to store the list of possible answers
-      var answers = [];
-
-      // and for each available answer...
-      for(letter in currentQuestion.answers){
-
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
-
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
-      );
-    }
-  );
-  // finally combine our output list into one string of HTML and put it on the page
-  quizContainer.innerHTML = output.join('');  
-}
-
-function showResults() {}
-
-var quizContainer = document.getElementById(".quiz");
-var resultsContainer = document.getElementById(".results");
-var submitButton = document.getElementById(".submit");
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("responses");
+var submitButton = document.getElementById("submit");
 var listOfQuestions = [
     {
     question: "Who invented JavaScript?",
     answers: {
-      a: "Douglas Crockford",
-      b: "Sheryl Sandberg",
-      c: "Brendan Eich"
+      1: "Douglas Crockford",
+      2: "Sheryl Sandberg",
+      3: "Brendan Eich"
     },
     correctAnswer: "c"
   },
   {
     question: "Which one of these is a JavaScript package manager?",
     answers: {
-      a: "Node.js",
-      b: "TypeScript",
-      c: "npm"
+      1: "Node.js",
+      2: "TypeScript",
+      3: "npm"
     },
     correctAnswer: "c"
   },
   {
     question: "Which tool can you use to ensure code quality?",
     answers: {
-      a: "Angular",
-      b: "jQuery",
-      c: "ESLint",
+      1: "Angular",
+      2: "jQuery",
+      3: "ESLint",
     },
     correctAnswer: "c"
 }
 ];
 
+function showQuestions() {
+    timerCounter = 10;
+    console.log("start quiz time");
+    startTimer();
+    function startTimer () {
+        countDown = setInterval(function() {
+            timerCounter--;
+            timeCountDown.textContent = timerCounter;
+            if (timerCounter !== 0) {
+                  // variable to store the HTML output
+                var output = [];
 
-showQuestions();
+                // for each question...
+                listOfQuestions.forEach(
+                    (currentQuestion, questionNumber) => {
 
-submitButton.addEventListener("click", showResults);
+                    // variable to store the list of possible answers
+                    var answers = [];
 
+                    // and for each available answer...
+                    for(letter in currentQuestion.answers){
 
+                        // ...add an HTML radio button
+                        answers.push(
+                            `<label>
+                            <input type="button" name="question${questionNumber}" value="${letter}">
+                            ${letter} :
+                            ${currentQuestion.answers[letter]}
+                        </label>`
+                        );
+                    }
 
-
-
-
-
-// loadQuestions();
-
-// //The init function is called to upload any stored High Score
-// function init() {
-//     getHighScores();
-// }
-
-// //This starts the quiz itself
-// function loadQuestions() {
-//     timerCounter = 10;
-//     console.log("start quiz time");
-//     startTimer();
-//     // jumpToQuestions();
-// }
-
-
-//The timer interval that countdowns and return to page for score and initials input
-// function startTimer () {
-//     countDown = setInterval(function() {
-//         timerCounter--;
-//         timeCountDown.textContent = timerCounter;
-//         if (timerCounter !== 0) {
-//             console.log("jump to questions");
-//             clearInterval(countDown);
-//             renderQuestions();
-//         // } if (timerCounter === 0) {
-//         //     clearInterval(countDown);
-//         //     return;
-//             // window.open("initials.html","_self");
-//         }
-//     }, 1000);
-// }
-
-function renderQuestions() {
-    chosenQuestion = listOfQuestions[Math.floor(Math.random() * listOfQuestions.length)];
-    window.textContent = chosenQuestion;
-    for (var i = 0; i < listOfQuestions.length; i++) {
-        if (userInput = listOfQuestions[i].answer) {
-            keepScore++;
-            // alert("Correct!");
-        } else {
-            alert("Incorrect!");
-        }
+                    // add this question and its answers to the output
+                    output.push(
+                        `<div class="question"> ${currentQuestion.question} </div>
+                        <div class="answers"> ${answers.join('')} </div>`
+                    );
+                    }
+                );
+                // finally combine our output list into one string of HTML and put it on the page
+                quizContainer.innerHTML = output.join(''); 
+                console.log("jump to questions");
+                clearInterval(countDown);
+                showQuestions();
+                    } else if (timerCounter === 0) {
+                        clearInterval(countDown);
+                        return;
+                        window.open("initials.html","_self");
+                    }
+        }, 1000);
     }
 }
+
+
+function showResults() {
+  // gather answer containers from our quiz
+  var answerContainers = quizContainer.querySelectorAll('.answers');
+
+  // keep track of user's answers
+  numCorrect = 0;
+
+  // for each question...
+  listOfQuestions.forEach( (currentQuestion, questionNumber) => {
+
+    // find selected answer
+    var answerContainer = answerContainers[questionNumber];
+    var selector = `input[name=question${questionNumber}]:onclick`;
+    var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    // if answer is correct
+    if(userAnswer === currentQuestion.correctAnswer){
+      // add to the number of correct answers
+      numCorrect++;
+
+      // color the answers green
+      answerContainers[questionNumber].style.color = 'lightgreen';
+    }
+    // if answer is wrong or blank
+    else{
+      // color the answers red
+      answerContainers[questionNumber].style.color = 'red';
+    }
+  });
+
+  // show number of correct answers out of total
+  resultsContainer.innerHTML = `${numCorrect} out of ${listOfQuestions.length}`;
+}
+
+// //This starts the quiz itself
+function loadQuestions() {
+    timerCounter = 10;
+    console.log("start quiz time");
+    startTimer();
+    // jumpToQuestions();
+}
+
